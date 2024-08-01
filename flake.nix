956be7da5b44e488b5345e07d703df52e9573ce1
Marvin Krause <public@mkrause.org>
2024-08-01 18:48:45 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,32 +5,55 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     clj-nix.url = "github:jlesquembre/clj-nix";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, clj-nix }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        {
-          packages = {
-            default = clj-nix.lib.mkCljApp {
-              pkgs = nixpkgs.legacyPackages.${system};
-              modules = [
-                # Option list:
-                # https://jlesquembre.github.io/clj-nix/options/
-                {
-                  projectSrc = ./.;
-                  name = "uniref";
-                  main-ns = "uniref.core";
-                }
-              ];
-            };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    clj-nix,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      javaVersion = 22;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      checks = {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
+            cljfmt.enable = true;
+            typos.enable = true;
           };
+        };
+      };
 
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [ clojure ];
-          };
-        });
+      overlays.default = final: prev: rec {
+        jdk = prev."jdk${toString javaVersion}";
+        clojure = prev.clojure.override {inherit jdk;};
+      };
+
+      packages = {
+        default = clj-nix.lib.mkCljApp {
+          inherit pkgs;
+          modules = [
+            # Option list:
+            # https://jlesquembre.github.io/clj-nix/options/
+            {
+              projectSrc = ./.;
+              name = "uniref";
+              main-ns = "uniref.core";
+            }
+          ];
+        };
+      };
+
+      devShells.default = pkgs.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+        buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        packages = with pkgs; [clojure jdk cljfmt];
+      };
+    });
 }
